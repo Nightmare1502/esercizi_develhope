1503d0c726ea477f4d49ca6112b83c93068521ae
Enrico <enri.bellagamba@gmail.com>
2025-04-06 15:33:53 +0200

    Esercizio Record aggiunto

---
+++ IdeaProjects/Record/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Posizione pos1 = new Posizione(15.0, 30.0);
+
+        System.out.println("Le coordinate sono: x" + pos1.x() + " - y" + pos1.y());
+
+    }
+}
---
+++ IdeaProjects/Record/src/Posizione.java
@@ -0,0 +1,3 @@
+public record Posizione(Double x, Double y){
+    
+}
