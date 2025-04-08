bd40ab3ed5553d4e820d4c1ace239a547be0da63
Enrico <enri.bellagamba@gmail.com>
2025-04-08 20:17:07 +0200

    esercizio date 2 aggiunto

---
+++ IdeaProjects/DateDue/src/Main.java
@@ -0,0 +1,15 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        OffsetDateTime dataSelezionata = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println("La data selezionata è: " + dataSelezionata);
+
+        String dataString = dataSelezionata.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+        System.out.println("Data formattata: " + dataString);
+
+    }
+}
